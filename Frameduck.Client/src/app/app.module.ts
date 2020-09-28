23bce4141cd1199b2a118e55c83858e3f0e844e4
Nikolas Diesl <dieslnikolas@gmail.com>
2020-09-28 23:42:51 +0200

    first implementation of Db class

--- Frameduck.Client/src/app/app.module.ts
+++ Frameduck.Client/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule } from '@angular/core'; 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+// pages component
+import { AppComponent } from './homepage/index.component';
+
 
 @NgModule({
   declarations: [
