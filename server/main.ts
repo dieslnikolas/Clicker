6b5a958d41e462f22e27a340c153714482796a50
Nikolas Diesl <dieslnikolas@gmail.com>
2020-10-07 08:42:53 +0200

    added renderer

--- server/main.ts
+++ server/main.ts
@@ -17,7 +17,7 @@ function createWindow() {
 
   win.loadURL(
     url.format({
-      pathname: path.join(__dirname, `/../../dist/angular-electron/index.html`),
+      pathname: path.join(__dirname, '/../Frameduck/index.html'),
       protocol: 'file:',
       slashes: true,
     })
