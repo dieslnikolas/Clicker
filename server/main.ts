0d09036421c057b87db342ffc805b9a4b3c5ab70
Nikolas Diesl <dieslnikolas@gmail.com>
2020-10-07 09:09:28 +0200

    #64

--- server/main.ts
+++ server/main.ts
@@ -17,7 +17,7 @@ function createWindow() {
 
   win.loadURL(
     url.format({
-      pathname: path.join(__dirname, '/../Frameduck/index.html'),
+      pathname: path.join(__dirname, '/../client/index.html'),
       protocol: 'file:',
       slashes: true,
     })
