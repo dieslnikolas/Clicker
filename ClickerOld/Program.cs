487d24e7423f64934791b0360231b4f1be7530b2
Nikolas Diesl <dieslnikolas@gmail.com>
2021-06-03 23:16:28 +0200

    #89

--- ClickerOld/Program.cs
+++ ClickerOld/Program.cs
@@ -1,10 +1,12 @@
-using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Clicker
 {
@@ -16,10 +18,11 @@ namespace Clicker
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(builder =>
-                {
-                    builder.UseModularStartup<Startup>();
-                });
+              Host.CreateDefaultBuilder(args)
+                  .ConfigureWebHostDefaults(webBuilder =>
+                  {
+                      webBuilder.UseStartup<Startup>();
+                  });
+
     }
 }
