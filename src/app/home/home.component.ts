d73478288b21dc1b25676b7e39250919d0a51d71
Nikolas Diesl <dieslnikolas@gmail.com>
2021-07-20 23:12:53 +0200

    #89

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ElectronService } from '../core/services';
 
 @Component({
   selector: 'app-home',
