c33d9b247e41518f5beec91c4dc55865ac170b6f
Nikolas Diesl <dieslnikolas@gmail.com>
2021-07-27 07:56:27 +0200

    #89

--- src/app/window.ts
+++ src/app/window.ts
@@ -79,8 +79,7 @@ export class WindowBuilder {
         // Packed inside electron shell 
         else {
             // Path when running electron executable
-            // let pathIndex = './index.html';
-            let pathIndex = '';
+            let pathIndex = './index.html';
 
             // Path when running electron in local folder
             if (fs.existsSync(path.join(__dirname, '../../dist/index.html'))) {
