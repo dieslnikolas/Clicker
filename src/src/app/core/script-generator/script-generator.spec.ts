d657a616c03b03e22365c7dd063e09b31b649e6c
Nikolas Diesl <dieslnikolas@gmail.com>
2022-03-17 11:23:28 +0100

    #103 fixed production build

--- src/src/app/core/script-generator/script-generator.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ScriptGenerator } from './script-generator';
-
-describe('ScriptGenerator', () => {
-  it('should create an instance', () => {
-    expect(new ScriptGenerator()).toBeTruthy();
-  });
-});
