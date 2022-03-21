73f02daa974cdffc1edbd429f98abc934653d582
Nikolas Diesl <dieslnikolas@gmail.com>
2022-03-21 09:02:51 +0100

    fixed windows

--- src/src/app/core/common/scripts/runners/powershell-runner.ts
+++ src/src/app/core/common/scripts/runners/powershell-runner.ts
@@ -11,24 +11,27 @@ import { ProjectService } from "../../../services/project/project.service";
 })
 export class PowershellRunner implements IScriptRunner {
 
+    private commnad: string = `pwsh`;
+
     constructor(private electronService: ElectronService, private projectService: ProjectService) { }
 
     Init(): ChildProcessWithoutNullStreams {
-        return this.electronService.childProcess.spawn("pwsh", [`${this.projectService.initCommand.Path}`], {
+        return this.electronService.childProcess.spawn(this.commnad, [`${this.projectService.initCommand.Path}`], {
             cwd: this.projectService.appPath
         });
     }
 
     Run(action: string, item: Command): any {
 
+        
+        // MIN powershell VERSION 7.2.1 >> dotnet tool install --global PowerShell
         // if (item.IsContext) {
-
-            this.projectService.saveTmp();
-            let path = this.electronService.path.resolve(this.projectService.appPath, "tmp.json");
-
-            return this.electronService.childProcess.spawn(`pwsh`, [`-Command`, `${this.projectService.initCommand.Path} && Get-Content "${path}" | ConvertFrom-JsonExtend | ${item.Path}`], {
-                cwd: this.projectService.appPath
-            });
+        this.projectService.saveTmp();
+        let path = this.electronService.path.resolve(this.projectService.appPath, "tmp.json");
+        // }
+        return this.electronService.childProcess.spawn(this.commnad, [`-Command`, `${this.projectService.initCommand.Path} && Get-Content "${path}" | ConvertFrom-JsonExtend | ${item.Path}`], {
+            cwd: this.projectService.appPath
+        });
     }
 
 }
