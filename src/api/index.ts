62ec328bc322b82a32e87bd8e730c4f86d191443
Nikolas Diesl <dieslnikolas@gmail.com>
2022-05-16 23:07:49 +0200

    Express added

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+import home from "./routes/home";
+app.use('/', home);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
