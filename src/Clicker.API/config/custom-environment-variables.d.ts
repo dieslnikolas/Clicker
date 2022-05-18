3b156814c6180a1615751943a0f947f3f022df42
Nikolas Diesl <dieslnikolas@gmail.com>
2022-05-18 23:27:35 +0200

    #7 - express js with controllers and openapi v3

---
+++ src/Clicker.API/config/custom-environment-variables.d.ts
@@ -0,0 +1,10 @@
+declare const _default: {
+    accessTokenPrivateKey: string;
+    accessTokenPublicKey: string;
+    refreshTokenPrivateKey: string;
+    refreshTokenPublicKey: string;
+    dbUri: string;
+    port: string;
+};
+export default _default;
+//# sourceMappingURL=custom-environment-variables.d.ts.map
